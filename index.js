18c7f538d4a22e8b4774990a66d315ca5f8c957c
khanglevan09-lgtm <khanglevan09@gmail.com>
2026-09-22 04:17:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Bot AFK Minecraft với tính năng nhấp chuột đang hoạt động!')
+  res.send('Bot AFK Minecraft đang hoạt động!')
 })
 
 app.listen(port, () => {
@@ -17,14 +17,15 @@ function createBot() {
     host: 'vangioinetwork.xyz',
     port: 25565,
     username: 'Kiru',
-    version: '1.21.4'
+    version: '1.21.4',
+    auth: 'offline'
   })
 
   bot.on('spawn', () => {
     console.log('[LOG] Bot đã vào server thành công!')
     
     setTimeout(() => {
-      bot.chat('/login Kiru2000@')
+      bot.chat('/l Kiru2000@')
       console.log('[LOG] Đã thực hiện lệnh: /login')
     }, 3000)
 
@@ -50,7 +51,11 @@ function createBot() {
   })
 
   bot.on('message', (message) => {
-    console.log('[SERVER CHAT]: ' + message.toString())
+    const text = message.toString();
+    // Chỉ in ra log nếu tin nhắn có chứa từ "Kiru" hoặc chứa tên của Bot
+    if (text.includes('Kiru') || text.includes(bot.username)) {
+      console.log('[SERVER CHAT]: ' + text);
+    }
   })
 
   bot.on('end', () => {
